62fb5969f030ccb25cd04184ab42be067862bd7e
DrewVargas <41808076+DrewVargas@users.noreply.github.com>
2019-07-03 20:22:32 -0400

    Add Google fonts, install styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Sliceline</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello Sliceline</h1>
+        <p>Sliceline</p>
+      </div>
+    </>
   );
 }
 
