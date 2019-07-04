c26e9effde030b2b57aa719b9369bf94c292228d
DrewVargas <41808076+DrewVargas@users.noreply.github.com>
2019-07-03 21:22:19 -0400

    Add banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { Navbar } from './components/Navbar';
+import { Banner } from './components/Banner';
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Raleway', sans-serif;
     margin: 0;
-    padding: 0;
   }
   h1, h2, h3 {
     font-family: 'Righteous', cursive;
@@ -18,7 +18,7 @@ function App() {
     <>
       <GlobalStyle />
       <Navbar />
-      <p>Sliceline</p>
+      <Banner />
     </>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import img from '../img/banner.jpg';
+
+export const Banner = styled.div`
+  height: 200px;
+  background-image: url(${img});
+  background-position: center;
+  background-size: cover;
+`;
