e6fa6c8bde5546edc3d298075729ec9e66c96192
DrewVargas <41808076+DrewVargas@users.noreply.github.com>
2019-07-04 23:43:48 -0400

    Add price

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,43 +1,58 @@
+export function formatPrice(price) {
+  return price.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+}
+
 export const foodItems = [
   {
     name: 'Rectangle Pizza',
     img: '../img/rectangle-pizza.jpg',
-    section: 'Pizza'
+    section: 'Pizza',
+    price: 12
   },
   {
     name: 'Egg Pizza',
     img: '../img/egg-pizza.jpg',
-    section: 'Specialty Pizza'
+    section: 'Specialty Pizza',
+    price: 15
   },
   {
     name: 'Calzone',
-    img: '../img/banner.jpg',
-    section: 'Calzone'
+    img: '../img/calzone.jpg',
+    section: 'Calzone',
+    price: 8
   },
   {
     name: 'Lattace Pizza',
     img: '../img/lattace-pizza.jpg',
-    section: 'Specialty Pizza'
+    section: 'Specialty Pizza',
+    price: 15
   },
   {
     name: 'Small Rectangle Pizza',
     img: '../img/rect-pizza.jpg',
-    section: 'Pizza'
+    section: 'Pizza',
+    price: 10
   },
   {
     name: 'Pepper Pizza',
     img: '../img/pepper-pizza.jpg',
-    section: 'Specialty Pizza'
+    section: 'Specialty Pizza',
+    price: 10
   },
   {
     name: 'Tiny Pizza',
     img: '../img/tiny-pizza.jpg',
-    section: 'Pizza'
+    section: 'Pizza',
+    price: 8
   },
   {
     name: 'Stuffed Pizza',
     img: '../img/stuffed-pizza.jpg',
-    section: 'Specialty Pizza'
+    section: 'Specialty Pizza',
+    price: 12
   }
 ];
 
